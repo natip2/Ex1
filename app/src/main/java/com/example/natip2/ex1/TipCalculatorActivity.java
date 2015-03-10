94fa820e2a15e099f8b89176fc7e250b7671a0e0
natip2 <natip2@gmail.com>
2015-03-10 21:52:40 +0200

    final 1

--- app/src/main/java/com/example/natip2/ex1/TipCalculatorActivity.java
+++ app/src/main/java/com/example/natip2/ex1/TipCalculatorActivity.java
@@ -1,7 +1,7 @@
 package com.example.natip2.ex1;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 //import android.text.Editable;
 //import android.text.TextWatcher;
 import android.view.Menu;
